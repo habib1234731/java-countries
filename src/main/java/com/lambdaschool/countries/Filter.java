c920f18efcf0031012ac2d1c86dab768fd149f2b
habib1234731 <habibrehman1234731@gmail.com>
2019-02-12 21:14:00 -0500

    created the filter interface, and getWithLetter method(not working)

---
+++ src/main/java/com/lambdaschool/countries/Filter.java
@@ -0,0 +1,5 @@
+package com.lambdaschool.countries;
+
+public interface Filter {
+    boolean filter (Country c);
+}
---
+++ src/main/java/com/lambdaschool/countries/NameController.java
@@ -0,0 +1,25 @@
+package com.lambdaschool.countries;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/names")
+public class NameController {
+    @RequestMapping("/all")
+    public ArrayList<Country> getAllCountries() {
+        CountriesApplication.countryList.countryList.sort((e1, e2) ->
+                e1.getName().compareToIgnoreCase(e2.getName()));
+        return CountriesApplication.countryList.countryList;
+    }
+
+    @RequestMapping("/begin")
+    public ArrayList<Country> getWithLetter(@RequestParam(value = "letter") char ch) {
+        CountriesApplication.countryList.countryList.sort((e1, e2) ->
+                e1.getName().toLowerCase().startsWith(Character.toString(ch).toLowerCase()).compareToIgnoreCase(e2.getName()));
+        return CountriesApplication.countryList.countryList;
+    }
+}
